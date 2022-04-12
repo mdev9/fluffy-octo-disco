77cc31f432270fe25775a374b4e11c7c07335d79
mdev9 <devriesmarijn05@gmail.com>
2022-04-12 12:11:40 +0200

    Ajout de touches ignorees

--- dactylo/dactylo.js
+++ dactylo/dactylo.js
@@ -1,4 +1,5 @@
 var texte = 'Les kiwis sont des fruits de plusieurs espèces de lianes du genre Actinidia, famille des Actinidiaceae. Ils sont originaires de Chine, notamment de la province de Shaanxi. On en trouve par ailleurs dans des climats dits montagnards tropicaux. En France, les kiwis de l\'Adour sont les seuls à disposer d\'une indication géographique protégée (IGP) et d\'un label rouge.';
+var listeTouchesIgnores = ['Shift', 'Alt', 'OS', 'CapsLock', 'Unidentified', 'Delete', 'Escape', 'Control', 'Tab', 'Enter', 'F12', 'F11', 'F10', 'F9', 'F8', 'F7', 'F6', 'F5', 'F4', 'F3', 'F2', 'F1', 'Insert', 'End', 'AltGraph', 'PageUp', 'PageDown', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp']
 var longueurTexte = texte.length;
 var index = 0;
 var listeCaracteresFormates = [];
@@ -30,8 +31,8 @@ window.onload = function () {
             majTexte();
             return; // si la touche est backspace, on n'éxécute pas le reste de la fonction
         }
-        if (e.key == 'Shift') {
-            return; // si la touche est shift, on n'éxécute pas le reste de la fonction
+        if (listeTouchesIgnores.includes(e.key)) {
+            return; // si la touche est une touche parmie la liste, on n'éxécute pas le reste de la fonction.
         }
         if (e.key == texte[index]) {
             classe = 'caractere_bon';
@@ -61,3 +62,8 @@ function majTexte() {
     span_texte_ecrit.innerHTML = texteEcrit;
 
 }
+
+
+// texteExcrit, texteRestant, texteReference
+// Mettre à jour variable texteRestant, se baser sur les variables index et texteReference
+// majTexte() --> mettre à jour affichage de texteEcrit et texteRestant
