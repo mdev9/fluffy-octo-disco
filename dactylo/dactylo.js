21dd3fcea5d1ef55dc98bdc4511c016a5f084b15
mdev9 <devriesmarijn05@gmail.com>
2022-04-18 20:27:12 +0200

    Amelioration de la dactylographie

--- dactylo/dactylo.js
+++ dactylo/dactylo.js
@@ -2,16 +2,21 @@ var texte = 'Les kiwis sont des fruits de plusieurs espèces de lianes du genre 
 var listeTouchesIgnores = ['Shift', 'Alt', 'OS', 'CapsLock', 'Unidentified', 'Delete', 'Escape', 'Control', 'Tab', 'Enter', 'F12', 'F11', 'F10', 'F9', 'F8', 'F7', 'F6', 'F5', 'F4', 'F3', 'F2', 'F1', 'Insert', 'End', 'AltGraph', 'PageUp', 'PageDown', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp']
 var longueurTexte = texte.length;
 var index = 0;
+var texteRestant = texte;
 var listeCaracteresFormates = [];
-
-
+var heureDebutChrono;
+var jeuCommence = false;
+var texteEcrit = '';
 
 window.onload = function () {
-    var span_texte = document.getElementById('span_texte');
+    var span_texte_restant = document.getElementById('span_texte_restant');
     var span_texte_ecrit = document.getElementById('span_texte_ecrit');
     var btn_recommencer = document.getElementById('btn_recommencer');
+    var span_mpm = document.getElementById('span_mpm');
 
-    span_texte.innerText = texte;
+
+    span_texte_restant.innerText = texteRestant;
+
 
 
     window.addEventListener('keydown', function (e) {
@@ -22,6 +27,9 @@ window.onload = function () {
         var caractereAffiche = caractere;
         var elementSupprime;
 
+        if (jeuCommence) {
+            heureDebutChrono = Date.now()
+        }
         if (e.key == 'Backspace') {
             var elementSupprime = listeCaracteresFormates.pop();
             if (!elementSupprime.includes('caractere_en_trop')) {
@@ -50,19 +58,54 @@ window.onload = function () {
         listeCaracteresFormates.push(caractereFormate);
         majTexte();
     });
+
+
+    function compteur() {
+        // Mots par minute = (nombre de charactères / 5) / temps
+        var temps = Date.now - heureDebutChrono;
+        temps = temps / 3600;
+        var mpm = (texteEcrit.length / 5) / temps;
+        return mpm;
+    }
+
+    function majTexte() {
+
+        var texteEcrit = '';
+        listeCaracteresFormates.forEach(element => {
+            texteEcrit += element;
+            texteRestant = texte.substring(index, longueurTexte) // Update texte restant
+            console.log(index);
+        });
+        span_texte_ecrit.innerHTML = texteEcrit;
+        span_texte_restant.innerHTML = texteRestant;
+        span_mpm.innerHTML = compteur();
+
+        if (texteRestant == '') {
+            finjeu()
+        }
+
+    }
+
 }
 
-function majTexte() {
 
-    var texteEcrit = '';
-    listeCaracteresFormates.forEach(element => {
-        texteEcrit += element;
-    });
-    span_texte_ecrit.innerHTML = texteEcrit;
+
+
+function accuidite() {
 
 }
 
+
+function score() {
+    // Score = MPM * Accuidité
+
+
+}
+
+function finjeu() {
+
+}
 
 // texteExcrit, texteRestant, texteReference
-// Mettre à jour variable texteRestant, se baser sur les variables index et texteReference
+// Mettre à jour variable texteRestant, se baser sur les variables index et texteReference en utilisant la méthode substring()
 // majTexte() --> mettre à jour affichage de texteEcrit et texteRestant
